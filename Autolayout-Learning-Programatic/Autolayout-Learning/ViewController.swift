8f7bd4de1cae22bace53265e4af36b38fb42ca25
Ming Tee <mtee@squareup.com>
2023-02-06 15:11:42 +1100

    Add code template for frame layout

--- Autolayout-Learning-Programatic/Autolayout-Learning/ViewController.swift
+++ Autolayout-Learning-Programatic/Autolayout-Learning/ViewController.swift
@@ -9,17 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func buttonTouchUpInside(_ sender: Any) {
-        label.text = textField.text 
-    }
-
-    @IBOutlet weak var label: UILabel!
-
-    @IBOutlet weak var textField: UITextField!
+    private let label = UILabel(frame: CGRect(x: 50, y: 50, width: 200, height: 44))
+    private let button = UIButton()
+    private let textField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addSubviews()
+        configureViews()
+    }
+
+    func addSubviews() {
+        [label, button, textField]
+            .forEach { view.addSubview($0) }
+    }
+
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ])
+    }
+
+    func configureViews() {
+        label.text = "Welcome to iOS Learning series"
+        button.setTitle("Update label", for: .normal)
+        textField.placeholder = "New text"
     }
 }
 
